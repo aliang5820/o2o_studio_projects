deb1e337f9ebfd2b2e78d1a8d7ce50b6b7dce283
aliang5820 <aliang5820@126.com>
2016-08-31 00:46:24 +0800

    新增多渠道打包配置

--- o2o_business_studio/fanwe_o2o_businessclient_23/src/main/java/com/fanwe/constant/ApkConstant.java
+++ o2o_business_studio/fanwe_o2o_businessclient_23/src/main/java/com/fanwe/constant/ApkConstant.java
@@ -1,11 +1,14 @@
 package com.fanwe.constant;
 
+import com.fanwe.businessclient.BuildConfig;
+
 public class ApkConstant {
 
     public static final boolean DEBUG = true;
 
-    //public static final String SERVER_API_URL_MID = "www.taozyi.com";
-    public static final String SERVER_API_URL_MID = "101.200.88.185";
+    public static final String SERVER_API_URL_MID = BuildConfig.API_HOST;//gradle配置环境
+    //public static final String SERVER_API_URL_MID = "120.24.186.128";//测试环境
+    //public static final String SERVER_API_URL_MID = "101.200.88.185";//正式环境
 
     public static final String SERVER_API_URL_PRE = "http://";
     public static final String SERVER_API_URL_END = "/mapi/index.php";
--- o2o_custom_studio/fanwe_o2o_47/src/main/java/com/fanwe/constant/ApkConstant.java
+++ o2o_custom_studio/fanwe_o2o_47/src/main/java/com/fanwe/constant/ApkConstant.java
@@ -1,11 +1,14 @@
 package com.fanwe.constant;
 
+import com.fanwe.o2o.newo2o.BuildConfig;
+
 public class ApkConstant {
 
     public static final boolean DEBUG = true;
 
-    //public static String SERVER_API_URL_MID = "o2o.fanwe.net";
-    public static String SERVER_API_URL_MID = "101.200.88.185";
+    public static String SERVER_API_URL_MID = BuildConfig.API_HOST;//gradle配置环境
+    //public static String SERVER_API_URL_MID = "120.24.186.128";//测试环境
+    //public static String SERVER_API_URL_MID = "101.200.88.185";//正式环境
 
     public static final String SERVER_API_URL_PRE = "http://";
     public static final String SERVER_API_URL_END = "/mapi/index.php";
